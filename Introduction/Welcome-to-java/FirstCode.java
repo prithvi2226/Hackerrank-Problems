1dce0a19db6655210bdc98b1eaaddc593ab41d2d
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2021-07-19 16:19:43 +0530

    day 1 introduction

---
+++ Introduction/Welcome-to-java/FirstCode.java
@@ -0,0 +1,23 @@
+/**
+ * Task : Welcome to java
+ * @author : Sheeba
+ * @created : 2021-07-19
+**/
+
+public class FirstCode {
+  public static void main(String[] args) {
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Java_Course/fundamentals/FirstCode.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ * Task : Demonstrate output in Java
+ * @author : Sheeba
+ * @created : 2021-07-19
+**/
+
+public class FirstCode {
+  public static void main(String[] args) {
+    System.out.println("This is java");
+    System.out.println("It works!");
+  }
+
+}
+  
