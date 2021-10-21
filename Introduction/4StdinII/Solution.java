98133bc248089d60b3502aa41d759ab8c59451b3
prithvi2226 <itsmeprithvi2000@gmail.com>
2021-10-21 23:50:09 +0530

    Java Stdin and Stdout II

---
+++ Introduction/4StdinII/Solution.java
@@ -0,0 +1,36 @@
+/** Take inputs as integers, double, String
+ * @author : wassgud42069
+ * @created : 2021-10-21
+**/
+
+import java.util.Scanner;
+
+public class Solution{
+  public static void main(String[] Args){
+    Scanner input = new Scanner(System.in);
+    //Scanner bruh = new Scanner(System.in);
+
+    int n = input.nextInt();
+    double d = input.nextDouble();
+    input.nextLine();
+    String s = input.nextLine();
+
+    System.out.println("String: " + s);
+    System.out.println("Double: " + d);
+    System.out.println("Int: " + n);
+ 
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
